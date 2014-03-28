a3ced4aaa665e4ffd8116075ed98b9599a0a0201
Kachakil <daniel.kachakil@11paths.com>
2014-03-28 16:49:41 +0100

    Fixed bug in authenticationHeaders

--- latch.c
+++ latch.c
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+ 
 
 #include "latch.h"
 
@@ -124,7 +124,7 @@ void authenticationHeaders(const char* pHTTPMethod, const char* pQueryString, ch
 
 	b64hash = sign_data(SecretKey, stringToSign);
 
-	len = strlen(AUTHORIZATION_HEADER_NAME) + strlen(AUTHORIZATION_METHOD) + strlen(AppId) + strlen(b64hash) + 4;
+	len = strlen(AUTHORIZATION_HEADER_NAME) + strlen(AUTHORIZATION_METHOD) + strlen(AppId) + strlen(b64hash) + 5;
 	authHeader = malloc(len);
 	snprintf(authHeader, len, "%s: %s %s %s", AUTHORIZATION_HEADER_NAME, AUTHORIZATION_METHOD, AppId, b64hash);
 
