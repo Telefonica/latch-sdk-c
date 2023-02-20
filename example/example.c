10feaf1139103ddbe3312e14afac2685880b9023
lmunoz-m <luciamunoz057@gmail.com>
2023-02-20 16:28:19 +0100

    added a sdk usage example

---
+++ example/example.c
@@ -0,0 +1,45 @@
+
+#include "../latch.h"
+#include <stdio.h>
+
+int main()
+{
+
+	const char *AppId = "APP_ID";
+	const char *SecretKey = "SECRET_KEY";
+
+	init(AppId, SecretKey);
+
+	char *responsePairId, *responsePair, *responseStatus, responseUnpair, responseLock, responseUnlock;
+
+	responsePairId = pairWithId("test@email.com");
+
+	printf("%s\n", responsePairId);
+
+	responsePair = pair("pairCode");
+
+	printf("%s\n", responsePair);
+
+	responseLock = lock("accountId");
+
+	printf("%s\n", responseLock);
+
+	responseUnlock = unlock("accountId");
+
+	printf("%s\n", responseUnlock);
+
+	responseStatus = status("accountId");
+
+	printf("%s\n", responseStatus);
+
+	responseUnpair = unpair("accountId");
+
+	printf("%s\n", responseUnpair);
+
+
+
+
+
+
+	return 0;
+}
